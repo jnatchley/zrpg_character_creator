cdaed375f1b4c296e4823501fd2a93d1aa8fc800
Jessica Atchley <jatchley@naver.com>
2020-10-19 16:07:46 -0400

    First commit, base of project

---
+++ character.py
@@ -0,0 +1,8 @@
+##
+
+my_character = {'name': '', 'age': 0, 'race': '', 'occupation': ''}
+
+print("Welcome to the ZRPG character creator! We are going to answer some questions to create a randomized character for your zrpg needs.")
+
+name = input("Name your character something fun:  ")
+print(name)
